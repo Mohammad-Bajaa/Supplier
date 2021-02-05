b1ab2bbd2063b01081ccea7c2df1ad3ce7966c92
Mohammad-Bajaa <75749165+Mohammad-Bajaa@users.noreply.github.com>
2021-02-05 22:58:06 +0200

    Update CategorySpecificationsController.cs

--- Controllers/CategorySpecificationsController.cs
+++ Controllers/CategorySpecificationsController.cs
@@ -90,12 +90,11 @@ namespace Supplier.Controllers
         }
 
         // POST: CategorySpecifications/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,isValued,CategoryId")] CategorySpecification categorySpecification)
         {
+          
             if (id != categorySpecification.Id)
             {
                 return NotFound();
