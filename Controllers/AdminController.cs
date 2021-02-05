290159a2fd124d63d5bbb0645d1fc01c080afd79
fater2 <56210266+fater2@users.noreply.github.com>
2021-02-05 05:35:12 +0200

    login name

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,22 +2,46 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Supplier.Data;
 using Supplier.Models;
 
 namespace Supplier.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly SupplierIdentityDbContext _adminContext;
 
+        private readonly SupplierDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AdminController(SupplierIdentityDbContext adminContext/*, UserManager<IdentityUser> userManager*/, SupplierDbContext context/*, RoleManager<IdentityRole> roleManager*/)
+        {
+            _context = context;
+            _adminContext = adminContext;
+            //_userManager = userManager;
+            //_roleManager = roleManager;
+
+        }
         public IActionResult Index()
         {
-            
+
+
+
+            var user = _adminContext.Users.FirstOrDefault(m => m.Id == GetUserId());
+            ViewData["UserFirstName"] = user.firstname;
+            //ViewData["LastName"] = user.LastName;
+
             return View();
         }
-      
 
+        private string GetUserId()
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
     }
 }
 
